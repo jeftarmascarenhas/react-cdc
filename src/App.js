cf087920ad0679ac041522a75b09722dc3048f8c
Jeftar <jeftarmascarenhas@hotmail.com>
2017-01-22 16:15:12 -0300

    study component reuse

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './pure-min.css';
 import './side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './components/InputCustomizado';
+import ButtonCustomizado from './components/ButtonCustomizado';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {lista: []};
-  }
+    this.state = {lista: [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
 
-  componentWillMount() {
+  }
+  /*Poderia usar o componentWillMount porem ele é carregado antes do render como
+  não tenho dados para mostrar antes do render() uso o componentDidMount que carrega após
+  o componente ser renderizado*/
+  componentDidMount() {
     console.log('WillMount');
     $.ajax({
       url:'http://cdc-react.herokuapp.com/api/autores',
@@ -19,6 +28,38 @@ class App extends Component {
         this.setState({lista: resposta});
       }.bind(this)
     });
+  }
+
+  enviaForm (evento) {
+    console.log({nome:this.state.name, email:this.state.email, senha:this.state.senha});
+    evento.preventDefault();
+    $.ajax({
+      url:'http://cdc-react.herokuapp.com/api/autores',
+      contentType:'application/json',
+      dataType: 'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+      success: function (resp) {
+        this.setState({lista: resp});
+        this.setState({nome:'', email:'', senha:''});
+        console.log('Dados enviado com sucesso', resp);
+      }.bind(this),
+      error: function (resp) {
+        console.log('Erro de envio tipo do erro:', resp);
+      }
+    });
+  }
+
+  setNome (evento) {
+    this.setState({nome: evento.target.value});
+  }
+
+  setEmail (evento) {
+    this.setState({email: evento.target.value});
+  }
+
+  setSenha (evento) {
+    this.setState({senha: evento.target.value});
   }
 
   render() {
@@ -47,23 +88,11 @@ class App extends Component {
             </div>
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label>
-                    <input id="nome" type="text" name="nome" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label>
-                    <input id="senha" type="password" name="senha"  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label></label>
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                  </div>
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <InputCustomizado label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+                  <InputCustomizado label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+                  <InputCustomizado label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
+                  <ButtonCustomizado type="submit" name="Gravar" className="pure-button pure-button-primary"/>
                 </form>
 
               </div>
@@ -79,7 +108,7 @@ class App extends Component {
                       {
                         this.state.lista.map(function(autor) {
                           return (
-                            <tr>
+                            <tr key={autor.id}>
                               <td>{autor.nome}</td>
                               <td>{autor.email}</td>
                             </tr>
---
+++ src/components/ButtonCustomizado.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ButtonCustomizado extends Component {
+    render () {
+        return (
+            <div className="pure-control-group">
+                <label></label>
+                <button type={this.props.type} className={this.props.className}>{this.props.name}</button>
+            </div>
+        );
+    }
+}
+
+export default ButtonCustomizado;
